a121f8302a5de0e7ffa2ffc377c150ea81d311be
joey <joey54739@gmail.com>
2026-03-13 10:25:33 +0000

    feat: setup postgresql database for production

--- server/core/settings.py
+++ server/core/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
 
 
 # Password validation
